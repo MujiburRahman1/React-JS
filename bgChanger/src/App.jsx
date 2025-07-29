a3eecb41d40f653cfab6d66e61f15bfcee60ba5a
Mujib ur Rahman <mujibkhan081@gmail.com>
2025-07-29 07:00:55 +0500

    Update App.jsx

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -4,11 +4,40 @@ function App() {
   const [color, setColor] = useState("olive")
 
   return (
-    // <p>Mujib</p>
+    
     <div className="w-full h-screen duration-200"
-      style={{backgoundColor: color}}
+    style={{backgroundColor: color}}
     >
-      {/* <p>khan</p> */}
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button 
+          className="outline-none px-4 py-1 rounded-full text-white font-semibold shadow-md"
+          style={{backgroundColor: "red"}}
+          onClick={() => setColor("red")}
+          >Red
+          </button>
+          <button 
+          className="outline-none px-4 py-1 rounded-full text-white font-semibold shadow-md"
+          style={{backgroundColor: "yellow"}}
+          onClick={() => setColor("yellow")}
+          >Yellow
+          </button>
+          <button 
+          className="outline-none px-4 py-1 rounded-full text-white font-semibold shadow-md"
+          style={{backgroundColor: "blue"}}
+          onClick={() => setColor("blue")}
+          >Blue
+          </button>
+          <button 
+          className="outline-none px-4 py-1 rounded-full text-white font-semibold shadow-md"
+          style={{backgroundColor: "green"}}
+          onClick={() => setColor("green")}
+          >Green
+          </button>
+          
+        </div>
+      </div>
+      
     </div>
   )
 }
